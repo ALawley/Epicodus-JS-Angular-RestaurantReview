16bef96268513e1ae96da5bd1dbd5dafd1c03cf0
Anna Lawley and Mary Warrington <mary.warrington@gmail.com>
2016-03-24 11:35:54 -0700

    change specialties to dropdown

--- app/edit-restaurant-details.component.ts
+++ app/edit-restaurant-details.component.ts
@@ -1,19 +1,21 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { Restaurant } from './restaurant.model';
+import { SpecialtiesComponent } from './specialties.component';
 
 @Component({
   selector: 'edit-restaurant-details',
   inputs: ['restaurant'],
   outputs: ['onEndEdit'],
+  directives: [SpecialtiesComponent],
   template: `
     <div class="restaurant-form">
       <h4>Edit Restaurant Information</h4>
-      <input [(ngModel)]="restaurant.name" class="input-lg">
-      <input [(ngModel)]="restaurant.specialty" class="input-lg">
-      <input [(ngModel)]="restaurant.address" class="input-lg">
-      <input [(ngModel)]="restaurant.price" type="text" class="input-lg">
-      <button (click)=endEdit() class="btn-success btn-lg add-button">Done Editing</button>
-      <button (click)=deleteRestaurant() class="btn-success btn-lg add-button">Delete Restaurant</button>
+      <input [(ngModel)]="restaurant.name" class="input-md">
+      <specialties (onSpecialtySelect)="updateSpecialty($event)"></specialties>
+      <input [(ngModel)]="restaurant.address" class="input-md">
+      <input [(ngModel)]="restaurant.price" type="text" class="input-md">
+      <button (click)=endEdit() class="btn-success btn-md add-button">Done Editing</button>
+      <button (click)=deleteRestaurant() class="btn-success btn-md add-button">Delete Restaurant</button>
     </div>
   `
 })
@@ -29,5 +31,9 @@ export class EditRestaurantDetailsComponent {
   deleteRestaurant() {
     this.restaurant.delete = true;
     this.onEndEdit.emit(this.restaurant);
+  }
+  updateSpecialty(specialtyValue: string) {
+    console.log("parent", specialtyValue);
+    this.restaurant.specialty = specialtyValue;
   }
 }
--- app/new-restaurant.component.ts
+++ app/new-restaurant.component.ts
@@ -1,15 +1,17 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { Restaurant } from './restaurant.model';
+import { SpecialtiesComponent } from './specialties.component';
 
 @Component({
   selector: 'new-restaurant',
   outputs: ['onSubmitNewRestaurant'],
+  directives: [SpecialtiesComponent],
   template: `
     <div class="restaurant-form">
       <h4>Add a Restaurant:</h4>
-      <form (submit)="addRestaurant(newName, newSpecialty, newAddress, newPrice)">
+      <form (submit)="addRestaurant(newName, newAddress, newPrice)">
         <input required placeholder="name" class="input-lg" #newName>
-        <input required placeholder="specialty" class="input-lg" #newSpecialty>
+        <specialties (onSpecialtySelect)="setSpecialty($event)"></specialties>
         <input required placeholder="address" class="input-lg" #newAddress>
         <input required placeholder="price" type="text" class="input-lg" #newPrice>
         <button type="submit" class="btn-success btn-lg add-button">Add</button>
@@ -19,21 +21,28 @@ import { Restaurant } from './restaurant.model';
 })
 
 export class NewRestaurantComponent {
+  public newRestaurantSpecialty: string = "Sandwiches";
   public onSubmitNewRestaurant: EventEmitter<string[]>;
   constructor() {
     this.onSubmitNewRestaurant = new EventEmitter();
   }
-  addRestaurant(newName: HTMLInputElement, newSpecialty: HTMLInputElement, newAddress: HTMLInputElement, newPrice: HTMLInputElement){
+  addRestaurant(newName: HTMLInputElement, newAddress: HTMLInputElement, newPrice: HTMLInputElement){
     var newRestaurantInputs: string[] = [];
     newRestaurantInputs.push(newName.value);
-    newRestaurantInputs.push(newSpecialty.value);
+
+    newRestaurantInputs.push(this.newRestaurantSpecialty);
+    debugger;
     newRestaurantInputs.push(newAddress.value);
     newRestaurantInputs.push(newPrice.value);
 
     this.onSubmitNewRestaurant.emit(newRestaurantInputs);
     newName.value="";
-    newSpecialty.value="";
     newAddress.value="";
     newPrice.value="";
+  }
+  setSpecialty(specialtyValue: string) {
+    console.log("parent", specialtyValue);
+    this.newRestaurantSpecialty = specialtyValue;
+    console.log("specialty set", this.newRestaurantSpecialty);
   }
 }
