b59b5e2a93b451cfe323a8a0b0d258370038772e
Anna Lawley and Mary Warrington <ALawley1@gmail.com>
2016-03-24 09:11:57 -0700

    Add new-restaurant.component

--- app/restaurant-list.component.ts
+++ app/restaurant-list.component.ts
@@ -1,17 +1,24 @@
 import { Component } from 'angular2/core';
 import { Restaurant } from './restaurant.model';
 import { RestaurantComponent } from './restaurant.component';
+import { NewRestaurantComponent } from './new-restaurant.component';
 
 @Component({
   selector: 'restaurant-list',
   inputs: ['restaurantList'],
-  directives: [RestaurantComponent],
+  directives: [RestaurantComponent, NewRestaurantComponent],
   template: `
     <restaurant-display *ngFor="#currentRestaurant of restaurantList" [restaurant]="currentRestaurant">
     </restaurant-display>
+    <new-restaurant (onSubmitNewRestaurant)="createRestaurant($event)"></new-restaurant>
   `
 })
 
 export class RestaurantListComponent {
   public restaurantList: Restaurant[];
+  createRestaurant(userInputArray: string[]): void {
+    this.restaurantList.push(
+      new Restaurant(userInputArray[0], userInputArray[1], userInputArray[2], userInputArray[3])
+    );
+  }
 }
