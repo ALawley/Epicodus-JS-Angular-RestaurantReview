46961c177928c926c72d81f6387232e7df42d665
Anna Lawley and Mary Warrington <ALawley1@gmail.com>
2016-03-24 08:42:24 -0700

    add app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from 'angular2/core';
+import { Restaurant } from './restaurant.model';
 
 @Component({
   selector: 'my-app',
   template: `
     <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
-    </div>  
+      <h1>Restaurant Reviews!</h1>
+    </div>
   `
 })
 
 export class AppComponent {
-
+  public restaurants: Restaurant[];
+  constructor() {
+    this.restaurants = [
+      new Restaurant("Lardo", "Sandwiches", "SE 12th & Hawthorne", "$"),
+      new Restaurant("Hapa", "Ramen", "SE 28th & Division", "$"),
+      new Restaurant("Guero", "Mexican", "SE 28th & Ankeny", "$"),
+      new Restaurant("The Woodsman", "New American", "SE 46th & Division", "$$"),
+      new Restaurant("Ava Gene's", "Italian", "SE 34th & Division", "$$$")
+    ]
+  }
 }
